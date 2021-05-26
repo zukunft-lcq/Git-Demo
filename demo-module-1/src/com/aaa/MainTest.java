d23eb3a1d546ada01ec27a40131c7a8bb329a81d
zukunft-lcq <2051728840@qq.com>
2021-05-27 00:03:43 +0800

    声明一下:尝试进行第一次更改

--- demo-module-1/src/com/aaa/MainTest.java
+++ demo-module-1/src/com/aaa/MainTest.java
@@ -3,7 +3,7 @@ package com.aaa;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("哈哈哈，Git 提交成功到GitHub上了吗");
-        System.out.println("哈哈哈，Git ");
+        System.out.println("哈哈哈，Git该喝酒 ");
 
     }
 }
